9aa31dfff9eac17a6089b21eb6c93b20c066c3bb
ejallier <erwann.jallier@afidium.com>
2023-10-11 18:07:10 +0200

    start

---
+++ movie-web/config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://movie-web.erwann-jallier.workers.dev/",
+  VITE_TMDB_READ_API_KEY: "dac978a6d3721c11e59401d05d74c8d5"
+};
