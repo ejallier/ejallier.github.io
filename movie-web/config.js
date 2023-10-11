40399b5ffba5905b50f84d284ef16d790b1ba422
ejallier <erwann.jallier@afidium.com>
2023-10-11 18:16:20 +0200

    fix paths

--- movie-web/config.js
+++ movie-web/config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
   VITE_CORS_PROXY_URL: "https://movie-web.erwann-jallier.workers.dev/",
-  VITE_TMDB_READ_API_KEY: "dac978a6d3721c11e59401d05d74c8d5"
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYWM5NzhhNmQzNzIxYzExZTU5NDAxZDA1ZDc0YzhkNSIsInN1YiI6IjY1MjZjNmY3ODEzODMxMDExYjQ4N2QwMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.629js4Z0R-aS-MZaL5p3XDoOdmHTU94ex2CZ9TiisNM"
 };
